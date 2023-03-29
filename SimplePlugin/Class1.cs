836fb70420b8d87c3fe0e2b3e2af4620772e58c2
Thomas Sandsør <33664322+thomassandsor@users.noreply.github.com>
2023-03-29 23:56:14 +0200

    Update

--- SimplePlugin/Class1.cs
+++ SimplePlugin/Class1.cs
@@ -18,11 +18,14 @@ namespace SimplePlugin
                 service = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(context.UserId);
                 tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
                 tracingService.Trace("First tracing test");
+                throw new InvalidPluginExecutionException("test");
 
             }
             catch (Exception ex)
             {
+                tracingService.Trace(ex.Message);
                 throw;
+                throw new InvalidPluginExecutionException(ex.Message);
             }
         }
     }
