c7052d9d14ce852d62dbf66c512c1ce5a7954c72
Thomas Sandsør <33664322+thomassandsor@users.noreply.github.com>
2023-03-31 08:33:29 +0200

    update

--- SimplePlugin/Class1.cs
+++ SimplePlugin/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace SimplePlugin
 {
@@ -7,26 +8,68 @@ namespace SimplePlugin
     {
         IOrganizationService service;
         Entity currentEntity;
+        Entity preImage;
         IPluginExecutionContext context;
         ITracingService tracingService;
 
         public void Execute(IServiceProvider serviceProvider)
         {
-            try
-            {
-                context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-                service = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(context.UserId);
-                tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-                tracingService.Trace("First tracing test");
-                throw new InvalidPluginExecutionException("test");
+            context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            service = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(context.UserId);
+            tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            currentEntity = (Entity)context.InputParameters["Target"];
+            tracingService.Trace("before image");
+            tracingService.Trace("target: " + currentEntity.LogicalName);
+            
 
-            }
-            catch (Exception ex)
+
+
+            if (context.PreEntityImages.Contains("PreImage2"))
             {
-                tracingService.Trace(ex.Message);
-                throw;
-                throw new InvalidPluginExecutionException(ex.Message);
+                preImage = context.PreEntityImages["PreImage2"];
+                tracingService.Trace("preimagebefore: " + context.PreEntityImages["PreImage2"].GetAttributeValue<int>("plugin_integera"));
             }
+            else
+            {
+                tracingService.Trace("PreImage not found");
+                return;
+            }
+            //preImage = context.PreEntityImages["PreImage"];
+
+                //Enter Custom Code
+                tracingService.Trace("First tracing test");
+                //throw new InvalidPluginExecutionException("test");
+
+                CalculateSum();
+
+        }
+        private void CalculateSum()
+        {
+            int sum = 0;
+            tracingService.Trace("Beginning Sum: " + sum);
+
+            tracingService.Trace("currentEntity.LogicalName: " + preImage.LogicalName);
+            tracingService.Trace("currentEntity.Attributes: " + string.Join(",", preImage.Attributes.Keys));
+
+            // Obtain the values of the integer fields
+            int fieldA = preImage.GetAttributeValue<int>("plugin_integera");
+            tracingService.Trace("Field A: " + fieldA);
+            int fieldB = preImage.GetAttributeValue<int>("plugin_integerb");
+            tracingService.Trace("Field B: " + fieldB);
+            
+            // Calculate the sum of the integer fields
+            sum = fieldA + fieldB;
+            tracingService.Trace("Total SUm: " + sum);
+
+            //currentEntity["plugin_resultintegercalculation"] = sum;
+
+            // Update the record in Dynamics 365
+            //service.Update(currentEntity);
+
+
+            Entity updatedEntity = new Entity(preImage.LogicalName, preImage.Id);
+            updatedEntity["plugin_resultintegercalculation"] = sum;
+            service.Update(updatedEntity);
         }
     }
 }
