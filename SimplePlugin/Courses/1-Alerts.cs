5fb757616f80978976ff94a649b8c90a4559db96
Thomas Sandsør <sandsor@outlook.com>
2023-03-31 13:08:06 +0200

    Course Updates

---
+++ SimplePlugin/Courses/1-Alerts.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace SimplePlugin
+{
+    public class SimplePlugin : IPlugin
+    {
+        IOrganizationService service;
+        Entity RecordAfterUpdate;
+        IPluginExecutionContext context;
+        ITracingService tracingService;
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            service = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(context.UserId);
+            tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            RecordAfterUpdate = context.PostEntityImages["PostImageMain"];
+
+
+            /* --------------    Enter Custom Code   ------------------------*/
+            tracingService.Trace("You are awesome! Congratulations on the first plugin");
+
+        }
+    }
+}
